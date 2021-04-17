ff09446296683a0b113a93bb11f2b6da2ebc20d9
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-04-16 23:57:49 -0300

    feat: modify height of sidebar and change global css

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
-import { Button } from './components/Button';
 import { MovieCard } from './components/MovieCard';
-
-// import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
 
 import './styles/global.scss';
 
-import './styles/sidebar.scss';
 import './styles/content.scss';
 
 import {
